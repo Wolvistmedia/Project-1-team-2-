e9bbbcd3c42f32820445f5b9edc44764cc2d9d04
Aman-wolvist <aman@wolvistmedia.com>
2026-04-09 19:46:35 +0530

    Frontend Backend Connect

--- backend/utils/contactSchema.js
+++ backend/utils/contactSchema.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 
 const contactSchema = Joi.object({
-    Name: Joi.string().required(),
+    name: Joi.string().required(),
     phone: Joi.string().required(),
     email: Joi.string().email().required(),
     message: Joi.string().required()
--- frontend/components/Form.tsx
+++ frontend/components/Form.tsx
@@ -1,13 +1,58 @@
 'use client'
 import { motion } from "framer-motion"
+import { useState } from "react"
 
 function Form() {
-    return (
-        <div
-        id="form"
-        className="w-full py-24 px-4 md:px-10 lg:px-20 bg-white">
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        message: ""
+    })
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const res = await fetch("http://localhost:8080/api/contact", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formData)
+            })
+
+            const data = await res.json()
+            console.log(data)
+
+            alert("Message sent ✅")
 
            
+            setFormData({
+                name: "",
+                email: "",
+                phone: "",
+                message: ""
+            })
+
+        } catch (err) {
+            console.log(err)
+            alert("Error ❌")
+        }
+    }
+    return (
+        <div
+            id="form"
+            className="w-full py-24 px-4 md:px-10 lg:px-20 bg-white">
+
+
             <motion.div
                 initial={{ opacity: 0, y: 30 }}
                 whileInView={{ opacity: 1, y: 0 }}
@@ -22,41 +67,46 @@ function Form() {
                 </p>
             </motion.div>
 
-         
+
             <motion.form
+                onSubmit={handleSubmit}
                 initial={{ opacity: 0, y: 40 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.6 }}
                 className="max-w-3xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 "
             >
 
-                
+
                 <input
                     type="text"
+                    name="name" onChange={handleChange}
                     placeholder="Your Name"
                     className="col-span-1 border border-gray-500 rounded-lg px-4 py-3 outline-none focus:border-black transition"
                 />
 
-              
+
                 <input
                     type="email"
+                    name="email" onChange={handleChange}
                     placeholder="Your Email"
                     className="col-span-1 border border-gray-500 rounded-lg px-4 py-3 outline-none focus:border-black transition"
                 />
 
                 <input
                     type="tel"
+                    name="phone" onChange={handleChange}
                     placeholder="Your Phone Number"
                     className="col-span-2 border border-gray-500 rounded-lg px-4 py-3 outline-none focus:border-black transition"
                 />
 
                 <textarea
                     rows={5}
+                    name="message" onChange={handleChange}
                     placeholder="Your Message"
                     className="col-span-2 border border-gray-500 rounded-lg px-4 py-3 outline-none focus:border-black resize-none transition"
                 ></textarea>
 
-               
+
                 <div className="col-span-2 flex justify-center">
                     <button
                         type="submit"
